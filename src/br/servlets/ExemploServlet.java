410a06225404726fa623cf55d3002670b7e4e25b
Lázaro Sobral <lazaro.sobral@facape.br>
2021-01-14 22:06:28 -0300

    Exmplo-NTI-Servlet

---
+++ src/br/servlets/ExemploServlet.java
@@ -0,0 +1,24 @@
+package br.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/exemploServlet")
+public class ExemploServlet extends HttpServlet  {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service (HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		writer.println("<html><body><h1>Exemplo Servlet NTI</h1></body></html>");
+	}
+}
+
